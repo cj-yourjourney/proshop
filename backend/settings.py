9b9e30cb72c6e5915b470e41d9eb448a5ea92194
cj herenow <cj@cjs-Mac-mini.local>
2024-02-16 10:22:29 -0800

    debug to ture

--- backend/settings.py
+++ backend/settings.py
@@ -241,5 +241,5 @@ AWS_S3_SECRET_ACCESS_KEY = os.environ.get('AWS_S3_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'myproshop-bucket'
 
 
-if os.getcwd() == '/app':
-    DEBUG = False
+# if os.getcwd() == '/app':
+#     DEBUG = False
